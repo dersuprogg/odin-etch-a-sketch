0874276962c80718c226b4ebd9cdb29fb1a83b7d
Dersu <148701933+dersuprogg@users.noreply.github.com>
2023-11-15 19:14:18 +0500

    make buttons for 8x8, 16x16, 32x32 board sizes

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const board = document.querySelector(".board");
+const sizeBtns = document.querySelectorAll(".sizes .btn");
 
 function makeCells(numberOfCells) {
+  board.innerHTML = ``;
   for (let i = 0; i < numberOfCells ** 2; i++) {
     const cellDiv = document.createElement("div");
     cellDiv.classList.add("cell");
@@ -10,15 +12,31 @@ function makeCells(numberOfCells) {
   }
 }
 
-makeCells(16);
-
 let cellColor = "yellow";
 
-const cells = document.querySelectorAll(".cell");
+function makeBoardPaintable() {
+  const cells = document.querySelectorAll(".cell");
+  console.log(this);
+  cells.forEach(function (cell) {
+    cell.addEventListener("mouseover", function () {
+      console.log("XD");
+      this.style.backgroundColor = cellColor;
+      this.style.borderColor = cellColor;
+    });
+  });
+}
 
-cells.forEach((cell) => {
-  cell.addEventListener("mouseenter", function () {
-    this.style.backgroundColor = cellColor;
-    this.style.borderColor = cellColor;
+sizeBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    if (this.classList.contains("eight")) {
+      makeCells(8);
+    }
+    if (this.classList.contains("sixteen")) {
+      makeCells(16);
+    }
+    if (this.classList.contains("thirtytwo")) {
+      makeCells(32);
+    }
+    makeBoardPaintable();
   });
 });
